72d3150ed02dea297bda5e28b5295083ae85f4c6
Michel Jansen <michel@lokku.com>
2012-11-05 16:02:17 +0000

    simpler initialisation and remove unused blocks

--- conditnio_app.rb
+++ conditnio_app.rb
@@ -13,29 +13,12 @@ module Conditnio
   class App < Sinatra::Base
     register Sinatra::ActiveRecordExtension
 
-    helpers do
-    end
-
-    configure do
-    end
-
     configure :development do
       set :database, 'sqlite:///development.db'
-      puts "database connection established"
     end
 
     configure :production do
-      # Database connection
-      db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/conditnio')
-      ActiveRecord::Base.establish_connection(
-        :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-        :host     => db.host,
-        :port     => db.port,
-        :username => db.user,
-        :password => db.password,
-        :database => db.path[1..-1],
-        :encoding => 'utf8'
-      )
+      set :database, ENV['DATABASE_URL']
     end
 
     get '/callback/:callback/:token' do
