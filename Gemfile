82ececce11e72cb639e2df292a75ed6a7acb4a8c
Michel Jansen <michel@lokku.com>
2012-11-05 11:23:18 +0000

    correctly init db

--- Gemfile
+++ Gemfile
@@ -5,3 +5,13 @@ gem "sinatra"
 gem "sinatra-contrib"
 gem "capistrano"
 gem "rspec"
+gem "activerecord"
+gem "sinatra-activerecord", require: "sinatra/activerecord"
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg' #postgres
+end
--- conditnio_app.rb
+++ conditnio_app.rb
@@ -1,16 +1,40 @@
 #encoding utf-8
 
-require "sinatra/reloader" if development?
+require 'bundler'
+Bundler.require
 
-class Conditnio::App < Sinatra::Base
+require 'uri'
 
-  helpers do
+require 'sinatra/reloader' if development?
+
+module Conditnio
+  class App < Sinatra::Base
+    register Sinatra::ActiveRecordExtension
+
+    helpers do
+    end
+
+    configure do
+    end
+
+    configure :development do
+      set :database, 'sqlite:///development.db'
+      puts "database connection established"
+    end
+
+    configure :production do
+      # Database connection
+      db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/conditnio')
+      ActiveRecord::Base.establish_connection(
+        :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+        :host     => db.host,
+        :port     => db.port,
+        :username => db.user,
+        :password => db.password,
+        :database => db.path[1..-1],
+        :encoding => 'utf8'
+      )
+    end
+
   end
-
-  configure do
-  end
-
-  configure :development do
-  end
-
 end
